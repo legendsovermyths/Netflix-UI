89f93347d270764414aadf8216836e816430e900
Mask Wolf <maskwolf2000@gmail.com>
2020-08-26 08:46:27 +0530

    add movie screen title

--- lib/MovieScreen.dart
+++ lib/MovieScreen.dart
@@ -28,15 +28,95 @@ class _MovieScreenState extends State<MovieScreen> {
                       clipper: CircularClipper(),
                       shadow: Shadow(blurRadius: 20.0),
                       child: Image(
-                        height: 350,
+                        height: 400,
                         width: double.infinity,
                         image:
                             NetworkImage(url + widget.movie['backdrop_path']),
                         fit: BoxFit.cover,
                       )),
                 ),
-              )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    padding: EdgeInsets.only(left: 30.0),
+                    onPressed: () => Navigator.pop(context),
+                    icon: Icon(Icons.arrow_back),
+                    color: Colors.black,
+                  ),
+                  Image(
+                    image: AssetImage('images/netflix_logo.png'),
+                    height: 60,
+                    width: 150,
+                  ),
+                  IconButton(
+                    padding: EdgeInsets.only(left: 30.0),
+                    onPressed: () => print("add to favourites"),
+                    icon: Icon(Icons.favorite_border),
+                    color: Colors.black,
+                  ),
+
+                ],
+              ),
+              Positioned.fill(
+                bottom: 10.0,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: RawMaterialButton(
+                    padding: EdgeInsets.all(10.0),
+                    elevation: 12,
+                    onPressed: () => print("Play"),
+                    shape: CircleBorder(),
+                    fillColor: Colors.white,
+                    child: Icon(
+                      Icons.play_arrow,
+                      size: 60,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0.0,
+                left: 20.0,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                  iconSize: 40.0,
+                  color: Colors.black,
+                ),
+              ),
+              Positioned(
+                bottom: 0.0,
+                right: 25.0,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.share),
+                  iconSize: 35.0,
+                  color: Colors.black,
+                ),
+              ),
             ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  widget.movie['title'] != null
+                      ? widget.movie['title'].toUpperCase()
+                      : widget.movie['name'].toUpperCase(),
+                  style:
+                      TextStyle(fontSize: 20.0, fontWeight: (FontWeight.bold)),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 10.0,
+                )
+              ],
+            ),
           )
         ],
       ),
